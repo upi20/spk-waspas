fed48c6ac6b06203e655f30a475dc5039d240846
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-03-22 18:50:59 +0700

    Update routing

--- pages/route.php
+++ pages/route.php
@@ -1,11 +1,32 @@
 <?php
 $route = true;
-$page_pilih_kasus = "./pages/kasus/pilih_kasus.php";
 
 if (!isset($_GET['kasus'])) {
-    require_once $page_pilih_kasus;
+    require_once "./pages/kasus/pilih_kasus.php";
+    die;
 }
 
 if (!isset($_GET['page'])) {
-    require_once $page_pilih_kasus;
+    require_once "./pages/dashboard/dashboard.php";
+} else {
+    switch ($_GET['page']) {
+        case 'dashboard':
+            break;
+
+        case 'kasus':
+            require_once "./pages/kasus/kasus.php";
+            break;
+
+        case 'alternatif':
+            require_once "./pages/alternatif/alternatif.php";
+            break;
+
+        case 'kriteria':
+            require_once "./pages/kriteria/kriteria.php";
+            break;
+
+        default:
+            require_once "./pages/dashboard/dashboard.php";
+            break;
+    }
 }
